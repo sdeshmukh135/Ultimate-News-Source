a40ebbf39a5bcd3e115537c16c6356652e998def
Sara Deshmukh <saradeshmukh@meta.com>
2025-06-24 16:38:32 -0700

    updated schema with News and Article, added logout and session functionality

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,44 @@
 const express = require('express')
 const cors = require('cors')
+const session = require('express-session')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+const { ValidationError } = require('./middleware/CustomErrors')
+
 const authRoutes = require('./routes/auth')
+const newsRoutes = require('./routes/news')
+const articleRoutes = require('./routes/articles')
 
 app.use(cors())
 app.use(express.json())
 
+let sessionConfig = {
+  name: 'sessionId',
+  secret: 'keep it secret, keep it safe',
+  cookie: {
+    maxAge: 1000 * 60 * 5,
+    secure: process.env.RENDER ? true : false,
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+}
 
 app.use((err, req, res, next) => {
     if (err instanceof ValidationError) {
       return res.status(err.statusCode).json({ error: err.message })
     }
 
-    // Additional Prisma error checks can be placed here
+    // For Additional Prisma Checks
     res.status(500).json({ error: "Internal Server Error" })
 })  
 
+app.use(session(sessionConfig))
 app.use(authRoutes)
+app.use(newsRoutes)
+app.use(articleRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
